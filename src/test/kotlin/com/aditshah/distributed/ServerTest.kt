322a5d53fc765cba93573d105a10d48a6394a661
Adit Shah <aditshah00@gmail.com>
2019-11-30 01:48:08 -0800

    Revert "0.1.1"
    This reverts commit 9ef78e7a73144cb94a8ada4da270087462316a29.

--- src/test/kotlin/com/aditshah/distributed/ServerTest.kt
+++ src/test/kotlin/com/aditshah/distributed/ServerTest.kt
@@ -1,55 +1,63 @@
 package com.aditshah.distributed
 
 
+import com.github.pambrose.common.util.sleep
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json
-import org.amshove.kluent.shouldEqual
-import org.amshove.kluent.shouldNotEqual
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import java.net.URL
-
+import kotlin.concurrent.thread
+import kotlin.time.seconds
 
 class ServerTest {
     @Test
     fun jsonServerTest() {
-        var json = URL("http://0.0.0.0:8080/drones").readText()
-        val last = Json.parse(LocationMapObj.serializer(), json)
-        json = URL("http://0.0.0.0:8080/drones").readText()
-        val locs = Json.parse(LocationMapObj.serializer(), json)
-        locs.map.size shouldEqual numDrones
-        last shouldNotEqual locs
-        MyDrone1.endAll()
+//        val info = MapSharedInfo(CoordinateArea(Coordinate(0, 0), Coordinate(100, 100)))
+//        val server = Server(8080, info)
+//        server.run()
+//        for (i in 1..3) {
+//            thread {
+//                val drone = MyDrone1(i, Coordinate(0, 0), info)
+//                while (true) {
+//                    sleep((i * 1).seconds)
+//                    drone.move()
+//                }
+//            }
+//        }
+//
+//        runBlocking {
+//            val client = HttpClient()
+//            val content = client.get<String>("localhost:8080/drones")
+//            println(content)
+//        }
+
+        sleep(1.seconds)
+        val json = URL("http://0.0.0.0:8080/drones").readText()
+        println(json)
+        //println("{\"1\":{\"X\":27,\"Y\":65,\"Z\":0},\"2\":{\"X\":23,\"Y\":83,\"Z\":0},\"3\":{\"X\":92,\"Y\":75,\"Z\":0}}\")"
+        val regex = Regex("""\{["\d+":\{"X":\d+,"Y":\d+,"Z":0},\}]+\}""")
+        //println(regex.matches("""{"1":{"X":38,"Y":10,"Z":0},"2":{"X":0,"Y":0,"Z":0},"3":{"X":0,"Y":0,"Z":0}}"""))
+        assert(regex.matches(json))
     }
 
-        //println(locs)
-        //println("{\"1\":{\"X\":27,\"Y\":65,\"Z\":0},\"2\":{\"X\":23,\"Y\":83,\"Z\":0},\"3\":{\"X\":92,\"Y\":75,\"Z\":0}}\")"
-        //val regex = Regex("""\{("\d":\{"X":\d+,"Y":\d+,"Z":\d+},)*("\d":\{"X":\d+,"Y":\d+,"Z":\d+})+}""")
-        //val regex = Regex("""\{("\d+":\{"X":\d+,"Y":\d+,"Z":\d+},){29}("\d+":\{"X":\d+,"Y":\d+,"Z":\d+})\}""")
-        //val parsedMap : HashMap<String,String> = Json.parse(json)
-        //working:
-        //val regex = Regex("""\{("\d+":\{"X":\d+,"Y":\d+,"Z":\d+},){""" + (numDrones - 1) + """}("\d+":\{"X":\d+,"Y":\d+,"Z":\d+})\}""")
-        //println(regex.matches("""{"1":{"X":38,"Y":10,"Z":0},"2":{"X":0,"Y":0,"Z":0},"3":{"X":0,"Y":0,"Z":0}}"""))
-        //assert(regex.matches(json))
-
-
-
     companion object {
-        const val numDrones = 50
         @JvmStatic
         @BeforeAll
         fun setUp() {
             val info = MapSharedInfo(CoordinateArea(Coordinate(0, 0), Coordinate(100, 100)))
             val server = Server(8080, info)
             server.run()
-//            val droneArray = AtomicReferenceArray<MyDrone1>(3)
-            for (i in 1..numDrones) {
-                val drone = MyDrone1(i, Coordinate(0, 0), info)
-//                droneArray[i-1] = drone
-                drone.start()
+            for (i in 1..3) {
+                thread {
+                    val drone = MyDrone1(i, Coordinate(0, 0), info)
+                    while (true) {
+                        sleep((i * 1).seconds)
+                        drone.move()
+                    }
+                }
             }
         }
 
@@ -60,9 +68,7 @@ class ServerTest {
                 val client = HttpClient()
                 client.get<String>("http://localhost:8080/shutdown")
             }
-//            for (i in 1..numDrones) {
-//                droneArray[i-1].stop()
-//            }
+
         }
     }
 }
